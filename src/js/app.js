2b87ba1580677075827548e8d68e09044fe5648e
OlegBirykov <p_petro75@mail.ru>
2020-10-04 22:40:15 +0300

    Добавлена заготовка кода клиентской части

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,56 @@
-// eslint-disable-next-line no-console
-console.log('it works!');
+const loginForm = document.querySelector('.login-form');
+const addForm = document.querySelector('.add-form');
+
+function authorization(event) {
+  event.preventDefault();
+
+  if (loginForm.elements.login.value.trim() === '') {
+    alert('Введите логин');
+    return;
+  }
+
+  if (loginForm.elements.password.value.trim() === '') {
+    alert('Введите пароль');
+    return;
+  }
+
+  loginForm.elements.login.value = '';
+  loginForm.elements.password.value = '';
+}
+
+function clearLogin(event) {
+  event.preventDefault();
+
+  loginForm.elements.login.value = '';
+  loginForm.elements.password.value = '';
+}
+
+function addQuote(event) {
+  event.preventDefault();
+
+  if (addForm.elements.quote.value.trim() === '') {
+    alert('Введите текст цитаты');
+    return;
+  }
+
+  if (addForm.elements.tag.value.trim() === '') {
+    alert('Введите категорию цитаты');
+    return;
+  }
+
+  addForm.elements.quote.value = '';
+  addForm.elements.tag.value = '';
+}
+
+function clearQuote(event) {
+  event.preventDefault();
+
+  addForm.elements.text.value = '';
+  addForm.elements.tag.value = '';
+}
+
+loginForm.addEventListener('submit', authorization);
+loginForm.addEventListener('reset', clearLogin);
+
+addForm.addEventListener('submit', addQuote);
+addForm.addEventListener('reset', clearQuote);
